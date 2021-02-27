b0c57484731acae219efcc48f7988e36b44775f0
William Angulo <wmavalle@gmail.com>
2021-02-27 16:23:29 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const Mensaje = (props) => {
+  console.log(props)
+ 
+  return<h1 style= {{color: props.color}}>
+    { props.message}
+    </h1>
+}
+const Descripcion = () => {
+  return<h2>Esta es la app de la bootcamp</h2>
+}
+const App =() =>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje color='green'message='Estamos trabajando en los cambios'></Mensaje>
+      <Mensaje color='red' message='En un curso'></Mensaje>
+      <Mensaje color='yellow'message='De react'></Mensaje>
+      <Descripcion></Descripcion>
+  
     </div>
   );
 }
